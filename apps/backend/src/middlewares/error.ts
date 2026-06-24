9353c2913b1303c1f86eced9d75874c70ef982aa
Zeeward41 <zeeward41@gmail.com>
2026-06-24 15:16:51 +0200

    refactor(middleware): update errorHandler to support OpenApiValidator and custom ErrorResponse

--- apps/backend/src/middlewares/error.ts
+++ apps/backend/src/middlewares/error.ts
@@ -1,17 +1,45 @@
 import type { Request, Response, NextFunction } from 'express';
+import ErrorResponse from '../utils/errorResponse';
+
+interface OpenApiValidator extends Error {
+    name: string;
+    status: number;
+    path: string;
+    errors: {
+        path: string;
+        message: string;
+        errorCode: string;
+    }[];
+}
 
 const errorHandler = (
-    err: Error,
-    req: Request,
+    err: ErrorResponse | OpenApiValidator,
+    _req: Request,
     res: Response,
-    next: NextFunction
+    _next: NextFunction
 ) => {
     //Log to console for dev
     console.log(err.stack?.red);
 
-    res.status(err.statusCode || 500).json({
+    let statusCode = 500;
+    let message = 'Server Error';
+
+    if (err instanceof ErrorResponse) {
+        statusCode = err.statusCode;
+        message = err.message;
+    } else {
+        statusCode = err.status;
+        const firstError = err.errors[0];
+        if (firstError) {
+            const cleanPath = firstError.path.startsWith('/')
+                ? firstError.path.slice(1)
+                : firstError.path;
+            message = `${cleanPath}: ${firstError.message}`;
+        }
+    }
+    res.status(statusCode).json({
         success: false,
-        error: err.message || 'Server Error',
+        error: message,
     });
 };
 
