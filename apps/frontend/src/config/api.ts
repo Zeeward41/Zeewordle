f39fa70350a0a91d5c209c0ba472c04d3a709dd8
Zeeward41 <zeeward41@gmail.com>
2026-07-10 16:14:17 +0200

    fix(frontend): update 'me' API route path

--- apps/frontend/src/config/api.ts
+++ apps/frontend/src/config/api.ts
@@ -1,7 +1,7 @@
 const API_URL = import.meta.env['VITE_API_URL'] as string;
 
 export const API_ROUTES = {
-    me: `${API_URL}/api/v1/auth/me`,
+    me: `${API_URL}/api/v1/me`,
     login: `${API_URL}/api/v1/auth/login`,
     logout: `${API_URL}/api/v1/auth/logout`,
 };
