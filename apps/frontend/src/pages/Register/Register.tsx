76c7966ad7d76b47708763be808734ea92f016ad
Zeeward41 <zeeward41@gmail.com>
2026-07-14 00:32:51 +0200

    feat(register): add form validation, API integration and tests

--- apps/frontend/src/pages/Register/Register.tsx
+++ apps/frontend/src/pages/Register/Register.tsx
@@ -1,7 +1,140 @@
 import './Register.css';
-import { Link } from '@tanstack/react-router';
+import { Link, useNavigate } from '@tanstack/react-router';
+import { useState } from 'react';
+import type { registerFieldType } from '../../schemas/auth.schema';
+import {
+    registerFieldSchema,
+    errorResponseSchema,
+} from '../../schemas/auth.schema';
+import { z } from 'zod';
+import { API_ROUTES } from '../../config/api';
+import { useNotification } from '../../hooks/useNotifications.ts';
+
+interface FormState {
+    status: 'idle' | 'loading' | 'success' | 'error';
+    message: string;
+}
 
 export const Register = () => {
+    const [field, setField] = useState<registerFieldType>({
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [fieldState, setFieldState] = useState<{
+        email: string;
+        username: string;
+        password: string;
+        confirmPassword: string;
+    }>({
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [formState, setFormState] = useState<FormState>({
+        status: 'idle',
+        message: '',
+    });
+    const { showNotification } = useNotification();
+    const navigate = useNavigate();
+
+    const handlerSubmit = async (
+        e: React.SubmitEvent<HTMLFormElement>
+    ): Promise<void> => {
+        e.preventDefault();
+
+        // Init
+        setFormState({
+            status: 'loading',
+            message: '',
+        });
+
+        // Validation des datas
+        setFieldState({
+            email: '',
+            username: '',
+            password: '',
+            confirmPassword: '',
+        });
+
+        const rawData = registerFieldSchema.safeParse(field);
+
+        // check if SafeParse succeed
+        if (!rawData.success) {
+            // format the errors
+            const fieldErrors = z.treeifyError(rawData.error);
+            // check properties exists
+            if (!fieldErrors.properties) return;
+            // get keys of the properties Object
+            const keyError = Object.keys(fieldErrors.properties);
+            // create an Object newErrors with key: string and value: string
+            const newErrors: Record<string, string> = {};
+
+            // shortcut of fieldErrors .properties -> properties with spécific type
+            const properties = fieldErrors.properties as Record<
+                string,
+                { errors: string[] }
+            >;
+
+            for (const key of keyError) {
+                const field = properties[key];
+                const errorMessage = field?.errors?.[0] ?? 'Unknown error';
+
+                newErrors[key] = errorMessage;
+            }
+
+            setFieldState(prev => ({
+                ...prev,
+                ...newErrors,
+            }));
+            return;
+        }
+
+        try {
+            // Fetch
+            const response = await fetch(API_ROUTES.register, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(rawData.data),
+                credentials: 'include',
+            });
+            const json = (await response.json()) as unknown;
+            if (!response.ok) {
+                const data = errorResponseSchema.parse(json);
+
+                const notifErr = {
+                    status: 'error' as const,
+                    message: `${data.message}`,
+                };
+                setFormState(notifErr);
+                showNotification(notifErr);
+                return;
+            }
+
+            const notifSuccess = {
+                status: 'success' as const,
+                message: 'You have successfully logged in.',
+            };
+            setFormState(notifSuccess);
+            showNotification(notifSuccess);
+            await navigate({ to: '/auth/login' });
+        } catch {
+            const notifErrNet = {
+                status: 'error' as const,
+                message: 'Network error, please try again.',
+            };
+            setFormState(notifErrNet);
+            showNotification(notifErrNet);
+        }
+    };
+
+    const handlerChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setField(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    };
     return (
         <div className="register-container">
             <div className="register">
@@ -9,7 +142,7 @@ export const Register = () => {
                 <p className="register__message">
                     Just a few quick things to get started
                 </p>
-                <form className="register-form">
+                <form className="register-form" onSubmit={handlerSubmit}>
                     <label
                         htmlFor="email"
                         className="register-form__label--email register-form__label"
@@ -21,12 +154,31 @@ export const Register = () => {
                         className="register-form__input--email register-form__input"
                         placeholder="Enter Email ID"
                         type="email"
+                        name="email"
+                        onChange={handlerChange}
+                        value={field.email}
                     />
                     <span className="register-form__error register-form__error--email">
-                        {' '}
-                        Ceci nest pas un mail
+                        {fieldState.email}
                     </span>
-
+                    <label
+                        htmlFor="username"
+                        className="register-form__label--username register-form__label"
+                    >
+                        Username
+                    </label>
+                    <input
+                        id="username"
+                        className="register-form__input--username register-form__input"
+                        placeholder="Enter Username"
+                        type="text"
+                        name="username"
+                        onChange={handlerChange}
+                        value={field.username}
+                    />
+                    <span className="register-form__error register-form__error--username">
+                        {fieldState.username}
+                    </span>
                     <label
                         htmlFor="password"
                         className="register-form__label--password register-form__label"
@@ -38,34 +190,38 @@ export const Register = () => {
                         className="register-form__input--password register-form__input"
                         placeholder="Enter New Password"
                         type="password"
+                        name="password"
+                        onChange={handlerChange}
+                        value={field.password}
                     />
                     <span className="register-form__error register-form__error--password">
-                        {' '}
-                        Ceci nest pas un password
+                        {fieldState.password}
                     </span>
 
                     <label
-                        htmlFor="passwordConfirm"
+                        htmlFor="confirmPassword"
                         className="register-form__label--passwordConfirm register-form__label"
                     >
                         Confirm Password
                     </label>
                     <input
-                        id="passwordConfirm"
-                        className="register-form__input--passwordConfirm register-form__input"
+                        id="confirmPassword"
+                        className="register-form__input--confirmPassword register-form__input"
                         placeholder="Enter Confirm Password"
                         type="password"
+                        name="confirmPassword"
+                        onChange={handlerChange}
+                        value={field.confirmPassword}
                     />
                     <span className="register-form__error register-form__error--passwordConfirm">
-                        {' '}
-                        Le password ne match pas
+                        {fieldState.confirmPassword}
                     </span>
 
                     <button
                         type="submit"
                         className="register-form__button register-form__button--submit"
                     >
-                        Sign Up{' '}
+                        {formState.status === 'loading' ? 'loading' : 'Sign Up'}
                     </button>
                 </form>
                 <div className="register__separator">
--- apps/frontend/src/schemas/auth.schema.ts
+++ apps/frontend/src/schemas/auth.schema.ts
@@ -29,7 +29,34 @@ export const notificationsSchema = z.object({
     message: z.string(),
 });
 
+// REGISTER
+
+export const registerFieldSchema = z
+    .object({
+        email: z
+            .string('the email must be a string')
+            .email('Invalid email format')
+            .max(100, 'The email must be less than 100 characters')
+            .toLowerCase(),
+        username: z
+            .string()
+            .min(5, { message: 'The username must be at least 5 characters' })
+            .max(15, {
+                message: 'The username must have a maximum of 15 characters',
+            }),
+        password: z
+            .string()
+            .min(5, 'The password must be at least 5 characters')
+            .max(100, 'The password must be at most 100 characters'),
+        confirmPassword: z.string(),
+    })
+    .refine(data => data.password === data.confirmPassword, {
+        message: 'Password do not match',
+        path: ['confirmPassword'],
+    });
+
 export type loginInput = z.infer<typeof loginSchema>;
 export type userSummaryType = z.infer<typeof userSummarySchema>;
 export type errorResponseType = z.infer<typeof errorResponseSchema>;
 export type notificationsType = z.infer<typeof notificationsSchema>;
+export type registerFieldType = z.infer<typeof registerFieldSchema>;
