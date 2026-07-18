30886e85b3cbac60b142bce22c91e1159638b702
Zeeward41 <zeeward41@gmail.com>
2026-07-18 11:25:25 +0200

    Add route and CSS styling for home page

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from '@tanstack/react-router';
 import './index.css';
+import { Home } from '../pages/Home/Home';
 
 export const Route = createFileRoute('/')({
     component: () => {
         return (
             <>
-                <p>Hello</p>
+                <Home />
             </>
         );
     },
