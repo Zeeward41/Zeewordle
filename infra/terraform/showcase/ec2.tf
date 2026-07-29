f321ff7948e8f47a66d4252fe981986990dff115
Zeeward41 <zeeward41@gmail.com>
2026-07-29 08:22:28 +0200

    feat(terraform): add security groups for monitoring proxy and app layers
    - Add 'ssm_instances_sg' for outbound SSM communication (HTTP/HTTPS)
    - Add 'monitoring_proxy' SG allowing inbound traffic on port 9090 (Prometheus UI)
    - Add 'app' SG allowing Node Exporter traffic (port 9100) from monitoring proxy

--- infra/terraform/showcase/ec2.tf
+++ infra/terraform/showcase/ec2.tf
@@ -3,15 +3,18 @@
 # ==============================================================================
 
 # First Spot instance managed by SSM
+# Monitory and proxy
 resource "aws_instance" "instance_1" {
   ami                         = var.ami_id
-  instance_type               = local.ec2_type
+  instance_type               = local.ec2_type_monitoring_proxy
   subnet_id                   = aws_subnet.main.id
   iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile.name
   user_data                   = local.user_data_script
+  key_name                    = aws_key_pair.ansible.key_name
   associate_public_ip_address = true
   vpc_security_group_ids = [
-    aws_security_group.ssm_instances_sg.id
+    aws_security_group.ssm_instances_sg.id,
+    aws_security_group.monitoring_proxy.id
   ]
 
   instance_market_options {
@@ -24,22 +27,24 @@ resource "aws_instance" "instance_1" {
 }
 
 # Second Spot instance managed by SSM
-# resource "aws_instance" "instance_2" {
-#   ami                  = var.ami_id
-#   instance_type        = local.ec2_type
-#   subnet_id            = aws_subnet.main.id
-#   iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-#   user_data            = local.user_data_script
-#
-#   vpc_security_group_ids = [
-#     aws_security_group.ssm_instances_sg.id
-#   ]
-#
-#   instance_market_options {
-#     market_type = "spot"
-#   }
-#
-#   tags = {
-#     Name = "ssm-spot-instance-2"
-#   }
-# }
+resource "aws_instance" "instance_2" {
+  ami                  = var.ami_id
+  instance_type        = local.ec2_type_app
+  subnet_id            = aws_subnet.main.id
+  key_name             = aws_key_pair.ansible.key_name
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
+  user_data            = local.user_data_script
+
+  vpc_security_group_ids = [
+    aws_security_group.ssm_instances_sg.id,
+    aws_security_group.app.id
+  ]
+
+  instance_market_options {
+    market_type = "spot"
+  }
+
+  tags = {
+    Name = "ssm-spot-instance-2"
+  }
+}
--- infra/terraform/showcase/security_groups.tf
+++ infra/terraform/showcase/security_groups.tf
@@ -1,5 +1,5 @@
 # ==============================================================================
-# SECURITY GROUPS
+# SECURITY GROUPS Common
 # ==============================================================================
 
 # Security group applied to EC2 instances allowing egress traffic for SSM management
@@ -43,4 +43,72 @@ resource "aws_vpc_security_group_egress_rule" "allow_ssm_http_outbound" {
   ip_protocol = "tcp"
   from_port   = 80
   to_port     = 80
+}
+
+# ===============================================================
+# Security Group for Proxy & Monitoring Host (Nginx + Prometheus)
+# ===============================================================
+
+resource "aws_security_group" "monitoring_proxy" {
+  name        = "monitoring-proxy-sg"
+  description = "Security group for Nginx reverse proxy and Prometheus monitoring server"
+  vpc_id      = aws_vpc.this.id
+
+  tags = {
+    Name = "monitoring-proxy-sg"
+  }
+}
+
+# ---------------------------------------------------------------
+# Ingress Rules (Inbound Traffic)
+# ---------------------------------------------------------------
+
+# Allow Web UI access to Prometheus
+resource "aws_vpc_security_group_ingress_rule" "allow_prometheus_ui_inbound" {
+  security_group_id = aws_security_group.monitoring_proxy.id
+  description       = "Allow inbound web traffic to Prometheus UI"
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port   = 9090
+  to_port     = 9090
+}
+
+# ===============================================================
+# Security Group for APP (Frontend, Backend, DB)
+# ===============================================================
+
+resource "aws_security_group" "app" {
+  name        = "app-sg"
+  description = "Security group for application layer (Frontend, Backend, DB)"
+  vpc_id      = aws_vpc.this.id
+
+  tags = {
+    Name = "app-sg"
+  }
+}
+
+# ---------------------------------------------------------------
+# Ingress Rules (Inbound Traffic)
+# ---------------------------------------------------------------
+
+# Allow Web UI access to Prometheus
+resource "aws_vpc_security_group_ingress_rule" "allow_prometheus_ui_inbound" {
+  security_group_id = aws_security_group.monitoring_proxy.id
+  description       = "Allow inbound traffic from monitoring_proxy on Port 9100 (Node Exporter) "
+
+  cidr_ipv4   = aws_instance.instance_1.public_ip
+  ip_protocol = "tcp"
+  from_port   = 9100
+  to_port     = 9100
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_node_exporter_from_monitoring" {
+  security_group_id = aws_security_group.app.id
+  description       = "Allow inbound traffic from monitoring_proxy on Port 9100 (Node Exporter)"
+
+  referenced_security_group_id = aws_security_group.monitoring_proxy.id
+  ip_protocol                  = "tcp"
+  from_port                    = 9100
+  to_port                      = 9100
 }
