165dc195b1d5cfc359251b64bc1881837403f274
Zeeward41 <zeeward41@gmail.com>
2026-07-31 03:34:20 +0200

    feat(terraform): enable automatic public IP assignment on EC2 instance

--- infra/terraform/showcase/ec2.tf
+++ infra/terraform/showcase/ec2.tf
@@ -29,12 +29,13 @@ resource "aws_instance" "instance_1" {
 
 # Second Spot instance managed by SSM
 resource "aws_instance" "instance_2" {
-  ami                  = var.ami_id
-  instance_type        = local.ec2_type_app
-  subnet_id            = aws_subnet.main.id
-  key_name             = aws_key_pair.ansible.key_name
-  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
-  user_data            = local.user_data_script
+  ami                         = var.ami_id
+  instance_type               = local.ec2_type_app
+  subnet_id                   = aws_subnet.main.id
+  key_name                    = aws_key_pair.ansible.key_name
+  iam_instance_profile        = aws_iam_instance_profile.ssm_instance_profile.name
+  user_data                   = local.user_data_script
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [
     aws_security_group.ssm_instances_sg.id,
